c6f1d96d9896fe2e9fa9af099444d224c49f6961
Dinh Ngoc Hien <hiendinhngoc@yahoo.com.vn>
2024-04-11 16:47:42 +0000

    Integrate rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -27,20 +27,48 @@ from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProces
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
-# HoneyComb
+# Cloudwatch logs
+import watchtower
+import logging
+from time import strftime
+
+# Rollbar
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
+# HoneyComb -- emplementation
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 
-# X-Ray
+# Rollbar -- emplementation
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
+
+# X-Ray -- emplementation
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-
-# Cloudwatch logs
-import watchtower
-import logging
-from time import strftime
 
 # Configuring Logger to Use CloudWatch
 # LOGGER = logging.getLogger(__name__)
@@ -61,7 +89,7 @@ app = Flask(__name__)
 # X-Ray
 # XRayMiddleware(app, xray_recorder)
 
-# HoneyComb
+# HoneyComb -- emplementation
 # Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
